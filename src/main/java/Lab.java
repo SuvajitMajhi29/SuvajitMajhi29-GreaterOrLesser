633aa63e534fb077fbde9258ceea6c99c2181234
SuvajitMajhi <suvajit.majhi19.work@gmail.com>
2024-09-13 15:55:25 +0530

    done

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -10,6 +10,14 @@ public class Lab {
      *          If neither is true, return "Equal". This problem is case-sensitive!
      */
     public String decide(int a, int b){
+        if (a>b) {
+            return "Greater";
+        }
+        else if(b>a)
+        {
+            return "Lesser";
+        }
+        else 
         return "Equal";
     }
 }
